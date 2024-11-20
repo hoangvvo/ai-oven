8708c4231768b200bf0d4a6b3ae29652ec927229
Hoang Vo <dev+g@hvvo.dev>
2024-11-20 18:12:43 +0700

    feat: implement sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,25 @@
+import { getProducts } from "@/lib/data";
+import type { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const products = await getProducts();
+  return [
+    {
+      url: `${process.env.APP_URL}/`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: `${process.env.APP_URL}/products`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    ...products.map((product): MetadataRoute.Sitemap[number] => ({
+      url: `${process.env.APP_URL}/products/${product.id}`,
+      changeFrequency: "weekly",
+      priority: 0.5,
+    })),
+  ];
+}
